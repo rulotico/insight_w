6737d73162ca38f82f48f643e8f8c03e47cf27df
rulotico <rulotico@gmail.com>
2015-10-15 18:25:34 -0500

    falla el side

---
+++ js/mains.js
@@ -0,0 +1,103 @@
+var h=$(window).height();
+$(".headpic").css("height",h)
+
+console.log(h);
+
+
+
+
+//Scroll
+
+var hh = h*.5;
+
+$( window ).scroll(function() {
+  				var s=$(window).scrollTop();
+  				console.log(s);
+          
+
+
+
+
+
+            if ( s > hh ) {
+              console.log("hola");
+              $(".side_r").animate({left:100},2000);
+              $(".side_l").animate({left:-90},2000);
+
+            };
+
+
+});
+
+
+
+
+
+
+
+
+// MENU TOOGLE 
+
+flagT=true;
+
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+       
+       	if (flagT==true) {
+       		 $(".logo").animate({left:-159},1000);
+       		 $(".social").animate({left:159},1200);
+       		 $(".ligh-flags").animate({left:20},1200);
+        	flagT=false;
+       	}else{
+       		$(".logo").animate({left:0},1000);
+       		$(".social").animate({left:314},900);
+       		$(".ligh-flags").animate({left:156},300);
+        	flagT=true;
+       	}
+
+    });
+
+
+//SOCIAL
+
+$("#face").mouseover(function(){
+
+$(this).attr("src","images/face_c.png");
+$(this).css("opacity",0);
+$(this).animate({opacity:1},500);
+
+
+
+});
+
+$("#twitter").mouseover(function(){
+
+$(this).attr("src","images/twitter_c.png")
+$(this).css("opacity",0);
+$(this).animate({opacity:1},500);
+
+});
+
+$("#face").mouseleave(function(){
+
+$(this).attr("src","images/face.png")
+$(this).css("opacity",0);
+$(this).animate({opacity:1},500);
+
+});
+
+$("#twitter").mouseleave(function(){
+
+$(this).attr("src","images/twitter.png")
+$(this).css("opacity",0);
+$(this).animate({opacity:1},500);
+
+});
+
+
+// SIDES
+
+
+
+
